2ed743c062c06afbedbf17381d44b2e3b580805c
dylan <7257923+dmnuggins@users.noreply.github.com>
2023-07-01 22:06:16 -0700

    Setup basic site structure with assets

---
+++ main.js
@@ -0,0 +1,38 @@
+console.log('Hello World!')
+
+
+
+/**
+ * Get user input:
+ *  - rock
+ *  - paper
+ *  - scissors
+ * Validate user input
+ * Generate computer choice (rock, paper, scissors)
+ * Compare userIn and compIn
+ * Decide winner based on combination of user & comp input
+ * Update points -> display updated score on site
+ * Prompt user for input
+ * IF user/comp reaches max wins (5) first, declare winner
+ */
+
+const images = document.querySelectorAll('.img-container img')
+
+console.log(images)
+
+// primary game loop
+function game() {
+
+};
+
+function playRound() {
+
+};
+
+function computerSelection() {
+    console.log('getComputerChoice')
+};
+
+function playerSelection() {
+    console.log('playerSelection')
+};
